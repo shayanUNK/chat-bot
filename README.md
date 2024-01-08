bed1b86c65d1064b4c6b03f6cbbe492eed3d228f
shayanUNK <143071027+shayanUNK@users.noreply.github.com>
2024-01-08 18:05:23 +0330

    Complete the code
    Write a readme. Write function for output. Optimizing the code.

--- README.md
+++ README.md
@@ -1,6 +1,37 @@
 # chat-bot
 
 ## About project
-This project is a chatbot that tries to use a series of **functional words** and perform a series of **mathematical** operations. You can see the commands of this bot in GitHub readme or index page.
+This project is a chatbot that tries to answer a series of **defined questions**. You can see the instructions for this bot in the GitHub readme or the index page.
 
 <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHRhWWHLQSe3jd_S398Eshj8xfxKgGyDbo9b7iZbodVab7bahcRXreNLmB-PH_-ackCCo&usqp=CAU" alt="">
+
+## commands
+### Persina commands:
+- سلام
+
+- چطوری؟
+
+- تو کی هستی؟
+
+- کار تو چیه؟
+
+- سازنده تو کیه؟
+
+- جواد کیه؟
+
+- کجا زندگی میکنی؟
+
+### English commands:
+- Hello
+
+- How are you?
+
+- Who are you?
+
+- what do you do?
+
+- who is your developer?
+
+- Who is Jawad?
+
+- Where do you live?
--- js/script.js
+++ js/script.js
@@ -7,6 +7,109 @@ Teacher: Mr. Khateri
 Email address: shayanunk@gmail.com
 Github: UNKshayan
 */
+// In script.js
+let input = document.querySelector(".main-input");
+let btn = document.querySelector(".main-button");
+let chat = document.querySelector(".chat-container");
+let form = document.querySelector(".form");
+
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
+  let text = input.value;
+  chat.insertAdjacentHTML("beforebegin", userTemplate(text));
+  setTimeout(() => {
+    chat.insertAdjacentHTML("beforebegin", robotTemplate(robotResponse()));
+    form.reset();
+  }, 1000);
+});
+
+function userTemplate(userMsg) {
+  let time = getTime();
+  return `
+  <div class="user">
+  <div class="time">
+    <span>${time}</span>
+  </div>
+    <span>${userMsg}</span>
+  </div>
+  `;
+}
+function robotTemplate(robotMsg) {
+  return `
+  <div class="robot">
+  <span>${robotMsg}</span>
+  </div>   
+  `;
+}
+function getTime() {
+  let toDay = new Date();
+  console.log(toDay);
+  let hours = toDay.getHours();
+  let min = toDay.getMinutes();
+
+  if (hours < 10) {
+    hours = "0" + hours;
+  }
+  if (min < 10) {
+    min = "0" + min;
+  }
+  let time = hours + ":" + min;
+
+  return time;
+}
+function robotResponse() {
+  let msg = input.value.toLowerCase();
+  if (msg.includes("hello")) {
+    return conversation[0][0].hello;
+  } else if (!msg) {
+    return "pleas ask me something";
+  } else if (msg.includes("سلام")) {
+    return conversation[0][1].helloFa;
+  } else if (msg.includes("how are")) {
+    return conversation[0][0].howAreYou;
+  } else if (
+    msg.includes("خوبی") ||
+    msg.includes("چطوری") ||
+    msg.includes("چخبر") ||
+    msg.includes("چطوره")
+  ) {
+    return conversation[0][1].howAreYouFa;
+  } else if (msg.includes("who are")) {
+    return conversation[1][0].whoAreYou;
+  } else if (
+    msg.includes("تو کی") ||
+    msg.includes("کی هستی") ||
+    msg.includes("کیستی")
+  ) {
+    return conversation[1][1].WhoAreYouFa;
+  } else if (msg.includes("you do")) {
+    return conversation[1][0].do;
+  } else if (msg.includes("کارت چیه") || msg.includes("چیکار میکنی")) {
+    return conversation[1][1].doFa;
+  } else if (msg.includes("your dev")) {
+    return conversation[1][0].developer;
+  } else if (
+    msg.includes("سازنت") ||
+    msg.includes("سازنده تو") ||
+    msg.includes("تو رو ساخته")
+  ) {
+    return conversation[1][1].developerFa;
+  } else if (msg.includes("commands")) {
+    return conversation[1][0].commands;
+  } else if (msg.includes("کامند") || msg.includes("دستورات")) {
+    return conversation[1][1].commandsFa;
+  } else if (msg.includes("jawad") || msg.includes("javad")) {
+    return conversation[1][0].jawad;
+  } else if (msg.includes("جواد")) {
+    return conversation[1][1].jawadFa;
+  } else if (msg.includes("live")) {
+    return conversation[1][0].city;
+  } else if (msg.includes("زندگی میکنی")) {
+    return conversation[1][1].cityFa;
+  } else{
+    return "Look at the commands and ask your question according to the given commands."
+  }
+}
 
 // list of conversation in chatBot
 const conversation = [
@@ -39,37 +142,30 @@ const conversation = [
     },
     // FARSI Repetition on questions(obj)
     {
-      WhoAreYouFa: "یک من ربات برای چت کردن و پرسیدن سوال های شما هستم",
+      WhoAreYouFa: " من یک ربات برای چت کردن و پرسیدن سوال های شما هستم",
       doFa: "من به یکسری از  سوال ها و چت های شما تا جایی که بشه جواب میدم و یکسری از عمل های ریاضی رو هم انجام میدهم.",
       developerFa:
         "سازنده من شایان محمودی است که درحال یادگیری جاوا اسکریپت است و این بات هم یک پروژه تمرینی است.",
       commandsFa:
-        "کامند های من رو میتونی در صفحه وبسایت یا readme صفحه گیتهاب ببینی",
+        "شما کامند های من رو میتونید در صفحه گیت هاب من یا همین صفحه ببینید و از آن استفاده کنید.",
       jawadFa:
         "جواد یک شخصیت است که هیچکس هنوز اون رو بجز استاد ندیده و نمیشناستش.",
       cityFa: "من در ایران در شهر بوشهر زندگی میکنم.",
     },
   ],
 ];
-function userInput() {
-  let num = 0;
-  while (num < 10) {
-    let Input = prompt("Ask your question");
-    if (Input) {
-      userOutput(Input);
-    } else {
-      userOutput();
-    }
-    num++;
-  }
-  alert("Refresh the page for ask another question")
-}
-userInput();
 
-function userOutput(output) {
-  if (!output) {
-    alert("Ask question");
-  } else {
-    alert(output);
-  }
-}
+// for (let index = 0; index < conversation.length; index++) {
+//   console.log(conversation[index]);
+//   for (let j = 0; j < index.length; j++) {
+//   }
+
+// }
+
+// for (let index = 0; index < conversation.length; index++) {
+//   const element = conversation[index];
+//   for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+
+//   }
+// }
